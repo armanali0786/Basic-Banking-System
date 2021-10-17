02c7a76baa5b3021ae487cca8b1535c3b267f371
Arman Ali <76746226+armanali0786@users.noreply.github.com>
2021-10-18 02:09:31 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : rgb(255, 160, 122);"><b>The Spark Bank</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="file:///C:/xampp/htdocs/WorldBankWebsite/worldbanksite/index.html" style="color : hsl(120, 100%, 25%);"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : hsl(120, 100%, 25%);"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : hsl(120, 100%, 25%);"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : hsl(120, 100%, 25%);"><b>Transaction Details</b></a>
+              </li>
+          </div>
+       </nav>
